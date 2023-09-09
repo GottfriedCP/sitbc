9bff6ac1031f724bd2548e232bdf0b89e4cee7ab
unknown <gottfriedcpn@gmail.com>
2023-09-09 22:21:51 +0700

    try make reset blob table

--- sitbc/management/commands/reset_blobb.py
+++ sitbc/management/commands/reset_blobb.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 
+from decouple import config
+
 import mysql.connector
 
 import os
@@ -12,4 +14,31 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        os.system("/usr/bin/php7.4 /home/gp/htdocs/csweb/ivtb/bin/console csweb:process-cases")
+        # Connect to the NL database
+        cnx = mysql.connector.connect(
+            user=config("MY_USER"),
+            password=config("MY_PASS"),
+            host=config("MY_HOST"),
+            database="dhsampel",
+        )
+
+        # Create a cursor object
+        cursor = cnx.cursor()
+
+        for table in (
+            "cases",
+            "cspro_jobs",
+            "cspro_meta",
+            "dhsampel_rec",
+            "level-1",
+            "notes",
+        ):
+            query = f"TRUNCATE TABLE {table}"
+            cursor.execute(query)
+            cnx.commit()
+
+        cursor.close()
+        cnx.close()
+
+        self.stdout.write(f"berhasil truncate dan buat ulang tabel blob")
+        # os.system("/usr/bin/php7.4 /home/gp/htdocs/csweb/ivtb/bin/console csweb:process-cases")
