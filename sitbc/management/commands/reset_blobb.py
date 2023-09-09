a5f5ee90d1b401cb35272a7565478a06ea347095
unknown <gottfriedcpn@gmail.com>
2023-09-09 22:44:20 +0700

    use real db

--- sitbc/management/commands/reset_blobb.py
+++ sitbc/management/commands/reset_blobb.py
@@ -14,33 +14,34 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        # Connect to the NL database
-        cnx = mysql.connector.connect(
-            user=config("MY_USER"),
-            password=config("MY_PASS"),
-            host=config("MY_HOST"),
-            database="dhsampel",
-        )
+        for db_name in ("sivtbc_ind_nl", "sivtbc_ind_l"):
+            # Connect to the NL database
+            cnx = mysql.connector.connect(
+                user=config("MY_USER"),
+                password=config("MY_PASS"),
+                host=config("MY_HOST"),
+                database=db_name,
+            )
 
-        # Create a cursor object
-        cursor = cnx.cursor()
-        cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+            # Create a cursor object
+            cursor = cnx.cursor()
+            cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
 
-        for table in (
-            "cases",
-            "cspro_jobs",
-            "cspro_meta",
-            "dhsampel_rec",
-            "level-1",
-            "notes",
-        ):
-            query = f"TRUNCATE TABLE `{table}`"
-            cursor.execute(query)
-        
-        cnx.commit()
-        cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
-        cursor.close()
-        cnx.close()
+            for table in (
+                "cases",
+                "cspro_jobs",
+                "cspro_meta",
+                "ind_nl_rec",
+                "level-1",
+                "notes",
+            ):
+                query = f"TRUNCATE TABLE `{table}`"
+                cursor.execute(query)
+            
+            cnx.commit()
+            cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
+            cursor.close()
+            cnx.close()
 
         os.system("/usr/bin/php7.4 /home/gp/htdocs/csweb/ivtb/bin/console csweb:process-cases")
         self.stdout.write(f"berhasil truncate dan buat ulang tabel blob")
